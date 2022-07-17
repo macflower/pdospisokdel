06488f1a2cc080db7d98014008428ee04c6ec774
Andy Granbill <stonemm21074mm@gmail.com>
2022-07-17 21:42:02 +0500

    PDO Список дел первый коммит

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta http-equiv="refresh" content="15">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Список дел</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+</head>
+<body>
+    <h1>Список дел</h1>
+</body>
+</html>
